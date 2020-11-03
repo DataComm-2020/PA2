e0c6ca1740b2fe1496edbadef9c8d92fdaa1138f
roshankc1018 <rk942@msstate.edu>
2020-11-03 14:24:33 -0600

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,327 +1,183 @@
-/****************************************************
- Roshan KC (rk942)
- Sunny Gurung (sg2094)
- Data Communication
- Professor - Maxwell Young
- Programming assigment 2: Client.cpp to demonstrate 
- GBN Protocol
- Due Date - October, 2020
-****************************************************/
-
-#include <stdlib.h>
-#include <cstring>
-#include <cstdlib>
-#include <iostream>
-#include <sys/types.h>
+/**************************************************
+ * Roshan KC (rk942) and Sunny Gurung (sg2094)
+ * 
+ * Programming Assignment 2
+ * This is a server program that sends files over UDP by using GBN protocol
+ * October 13, 2020
+ * ********************************************/
+#include <stdio.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <netdb.h>
 #include <iostream>
-#include <fstream>
-#include <arpa/inet.h>
-#include "packet.h"
-#include <math.h>
-#include <time.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h> 
+#include <fstream>
+#include "packet.h"
 #include "packet.cpp"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	//output the usage when length is not 4
-	if (argc != 4)
+  //output the usage when length is not 5
+	if (argc != 5)
 	{
-		printf("%s \n", "Usage: ./client localhost <port number> <filename>");
+		printf("%s \n", "Usage: ./server localhost <receive port number> <send port number> <filename>");
 		exit(1);
 	}
 
-
-
-	//server address
+  struct sockaddr_in client, server, emulatorSend;
+  socklen_t clen = sizeof(client);
+  socklen_t slen = sizeof(server);
+  socklen_t e_send = sizeof(emulatorSend);
+  
+  //sending to emulator address
 	struct hostent *s;
 	s = gethostbyname(argv[1]);
-	int portNo; //port number
-	struct sockaddr_in server;
-	socklen_t slen = sizeof(server);
-	portNo = atoi(argv[2]);
+	int sending_portNo = atoi(argv[3]);
 
-
-	if(portNo < 1024 || portNo > 65355){
-		cout << "Port Error!!!! Must be between 1024 and 65355!!" << endl;
+	if(sending_portNo < 1024 || sending_portNo > 65355){
+		cout << "Sending Port Error!!!! Must be between 1024 and 65355!!" << endl;
 		exit(1);
 	}
 
-	bzero((char *)&server, sizeof(server));
-	server.sin_family = AF_INET;	 //IPv4
-	server.sin_port = htons(portNo); //to network byte order
+	bzero((char *)&emulatorSend, sizeof(emulatorSend));
+	emulatorSend.sin_family = AF_INET;	 //IPv4
+	emulatorSend.sin_port = htons(sending_portNo); //to network byte order
 
 	bcopy((char *)s->h_addr,
-		  (char *)&server.sin_addr.s_addr,
+		  (char *)&emulatorSend.sin_addr.s_addr,
 		  s->h_length);
-
-	// setting up a socket
-	int udpSocket = 0;
-	if ((udpSocket = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-	{
-		std::cout << "Socket Error: " << strerror(errno) << " \n";
-		exit(1);
-	}
-
-	//file descriptor
-	FILE *file;					//holding the file to read
-	file = fopen(argv[3], "r"); // filename
-
-	//file descriptor
-	ofstream seqNumLog; //for clientsequm.log
-	ofstream ackLog;	//for clientack.log
-
-	seqNumLog.open("clientseqnum.log"); //opening file
-	ackLog.open("clientack.log");		//opening file
-
-	//file opened correctly
-	if (file == NULL)
-	{
-		cout << "Error opening file " << argv[3] << " !" << endl;
-		exit(1);
-	}
-	if (!seqNumLog.good())
-	{
-		std::cout << "Error opening file clientseqnum.log!" << std::endl;
-		exit(1);
-	}
-	if (!ackLog.good())
-	{
-		std::cout << "Error opening file clientack.log!" << std::endl;
-		exit(1);
-	}
-
-	int type = 0;	  //packet type
-	int seqnum = -1;  //packet seqnum
-	char data[8][31]; //2d array for holding 30 char data
-
-	//setting 2d data array to zero
-	for (int j = 0; j < 8; j++)
-	{
-		memset(data[j], '\0', sizeof(data[j]));
-	}
-
-	int length = 30;							  //packet length
-	char dataPacket[42];						  //data packet total length
-	memset(dataPacket, '\0', sizeof(dataPacket)); //emptying dataPacket Length
-
-	int nextSeqNum = 1; //next sequence number
-	int base = 0;		//base
-	int N = 7;			//window size
-
-	//creating packet
-	packet pack = packet(0, 0, 0, NULL);
-
-	//timer
-	struct timeval timer;
-
-	//setting up for select
-	fd_set fileDescriptors;
-	FD_ZERO(&fileDescriptors);
-	FD_SET(udpSocket, &fileDescriptors);
 	
-	bool endOfFileReached = false; //end of file reached
-	int outstandingPacket = 0;	   //no of outstanding packet at the given time
-	int receivedData[42];
-	cout << "-----------------------------------------" << endl;
-	while (1)
-	{
-		//if end of file is reached and outstanding packet is zero
-		if (endOfFileReached && outstandingPacket == 0)
-		{
-			//increase seqnum
-			seqnum++;
-			seqnum = seqnum % 8;
 
-			pack = packet(3, seqnum, 0, NULL); //make packet
-			pack.serialize(dataPacket);		   //serialize packet
+  // create socket first
+  int portno; // port number
+  portno = atoi(argv[2]);
+  if (portno < 1024 || portno > 65355)
+  {
+    perror("Port Error!!!! Must be between 1024 and 65355!!\n");
+    exit(1);
+  }
 
-			printf("Sending EOT packet to the server\n");
-			//send the packet
-			sendto(udpSocket, dataPacket, sizeof(dataPacket), 0, (struct sockaddr *)&server, sizeof(server));
+  server.sin_family = AF_INET;
+  server.sin_addr.s_addr = INADDR_ANY;
+  server.sin_port = htons(portno);
 
-			//print contents;
-			pack.printContents();
-			//write down the seqenece nubmer;
-			seqNumLog << pack.getSeqNum() << endl;
-		}
+  //data is sent over udp network
+  int udp_socket = 0;
+  if ((udp_socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+  { // socket(int domain, int type, int protocol)
+    cout << "Error in socket creation.\n";
+  }
+  server.sin_port = htons(portno); // set port number
 
-		//if outstanding packet is less than 7 and end of file is not reached
-		while (outstandingPacket < 7 && !endOfFileReached)
-		{
-			
-			//starting sequence number
-			seqnum++;
-			seqnum = seqnum % 8;
-			type = 1; 
+  if ((bind(udp_socket, (struct sockaddr *)&server, sizeof(server))) < 0) // if binding error occurs
+  {
+    perror("binding");
+    return -1;
+  }
 
+  // create out file to write character from a file
+  ofstream outfile;
+  outfile.open(argv[4]); // opens a file
+  if (!outfile.is_open())
+  {
+    perror("Outfile open error");
+  }
 
-			//setting to zero
-			memset(&dataPacket, 0, sizeof(dataPacket));
+  // ack file to write ack sent to the client
+  ofstream ackFile;
+  ackFile.open("arrival.log");
 
-			//reading a file
-			int readCount = fread(data[seqnum], 1, length, file);
-			if (readCount == length)
-			{	
+  if (!ackFile.is_open())
+  {
+    perror("Arrivalfile open error");
+  }
 
-				//making a packet
-				pack = packet(type, seqnum, length, data[seqnum]);
-				pack.serialize(dataPacket);	//serializing a packet
+  char receiveData[37]; // to receive from packet
+  char charData[30];    // character in the packet
+  char ackData[42];     // to send ack data to client
 
-				//send the message to server
-				sendto(udpSocket, dataPacket, sizeof(dataPacket), 0, (struct sockaddr *)&server, sizeof(server));
-				
+  int type = 1;
+  int seqnum = 0;
+  int acktype = 0;
+  int expectseq = 0;
 
-				//adding outstanding packets
-				outstandingPacket = (outstandingPacket + 1) % 8;
-				nextSeqNum = (seqnum + 1) % 8;
+  packet filepacket(type, seqnum, sizeof(charData), charData);
 
-				//printing contents
-				cout << outstandingPacket << endl;
-				pack.printContents();
-				cout << "SB: " << base << endl;
-				cout << "NS: " << nextSeqNum << endl;
-				cout << "Number of Outstanding Packets: " << outstandingPacket << endl;
-				cout << "-----------------------------------------" << endl;
-				
+  while (1)
+  {
+    memset(receiveData, 0, sizeof(receiveData));
+    memset(charData, 0, sizeof(charData));
+    memset(ackData, 0, sizeof(ackData));
 
-				//write to seqNumLog file
-				seqNumLog << pack.getSeqNum() << endl;
-			}
-			else
-			{
-				
-				data[seqnum][readCount] = '\0';
+    // error in receiving from client
+    if (recvfrom(udp_socket, receiveData, sizeof(receiveData), 0, (struct sockaddr *)&client, &clen) == -1)
+    {
+      perror("Receiving\n");
+      return -1;
+    }
 
-				//packing last data
-				pack = packet(type, seqnum, readCount, data[seqnum]);
+    filepacket.deserialize((char *)receiveData); // deserialize to see sent data
+    type = filepacket.getType();                 //get Type
+    seqnum = filepacket.getSeqNum();
 
-				//serializng the packet
-				pack.serialize(dataPacket);
+    ackFile << seqnum << "\n";
+    printf("\n--------------------------------------\n");
+    filepacket.printContents();
 
-				//send the message to server
-				sendto(udpSocket, dataPacket, sizeof(dataPacket), 0, (struct sockaddr *)&server, sizeof(server));
-				
-				//print contents
-				pack.printContents();
-				cout << "SB: " << base << endl;
-				cout << "NS: " << nextSeqNum << endl;
-				cout << "Number of Outstanding Packets: " << outstandingPacket + 1 << endl;
+    printf("\nExpecting Rn: %d\n", expectseq);
+    printf("sn: %d\n", seqnum);
 
-				//writing to the seqnumlog file
-				seqNumLog << pack.getSeqNum() << endl;
+    if (expectseq == seqnum) //  if expected sequence matches sequence number from client file
+    {
 
-				//updating outstanding Packet and nextseqnumber
-				outstandingPacket = (outstandingPacket + 1) % 8;
-				nextSeqNum = (seqnum + 1) % 8;
-				endOfFileReached = true;
-				break;
-			}
-		}
+      if (filepacket.getType() == 3) // if type sent by client is 3 then do EOT
+      {
 
-		//setting timers
-		timer.tv_sec = 2;
-		timer.tv_usec = 0;
+        acktype = 2; // set acktype to 2
+        packet ackpacket(acktype, seqnum, 0, 0);
+        ackpacket.serialize(ackData); //searialize to send data
+        if (sendto(udp_socket, ackData, sizeof(ackData), 0, (struct sockaddr *)&emulatorSend, sizeof(emulatorSend)) == -1)
+        {
+          perror("Sending Error");
+          return -1;
+        }
+        ackpacket.printContents(); //print sent data
+        printf("--------------------------------------\n");
+        break; // exit
+      }
 
-		//non-blocking listening
-		int m = udpSocket;
-		int setTimer = select(m + 1, &fileDescriptors, NULL, NULL, &timer);
+      outfile << filepacket.getData(); // write to the file
 
-		//udpSocket is ready
-		if (setTimer > 0)
-		{	
+      packet ackpacket(acktype, seqnum, 0, 0); // send only acktype and seq number
+      ackpacket.serialize((char *)ackData);    // searilize to send data in the socket
+      //send ack to server
+      if (sendto(udp_socket, ackData, sizeof(ackData), 0, (struct sockaddr *)&emulatorSend, sizeof(emulatorSend)) == -1)
+      {
+        printf("Sending Error\n");
+        return -1;
+      }
 
-			//receiving data
-			int c = recvfrom(udpSocket, receivedData, sizeof(receivedData), 0, (struct sockaddr *)&server, &slen);
-			if (c < 0)
-			{
+      ackpacket.printContents();
+      expectseq = (expectseq + 1) % 8;
+    }
+    else // if sequence number doesnot resend the ack to the server
+    {
+      packet ackpacket(acktype, expectseq, 0, 0);
+      ackpacket.serialize((char *)ackData);
+      sendto(udp_socket, ackData, sizeof(ackData), 0, (struct sockaddr *)&emulatorSend, sizeof(emulatorSend));
+      ackpacket.printContents();
+      printf("-------------------------------------\n");
+    }
+  }
 
-				perror("Receiving error\n");
-			}
+  outfile.close(); //close the file
 
-			//packing data
-			packet pack(0, 0, 0, NULL);
-			
-
-			//deserializing received data
-			pack.deserialize((char *)receivedData);
-
-			pack.printContents();
-
-			//if ack received is type 2
-			if (pack.getType() == 2)
-			{
-				printf("EOT packet received. Exiting----------------------------\n");
-				ackLog << pack.getSeqNum() << endl;
-				break;
-			}
-			else
-			{	
-				//updating base and outstanding packet
-				int ackseq = pack.getSeqNum();
-
-				if (ackseq >= base || (ackseq < (base + N) % 8 && ackseq >= 0))
-				{
-					base = (pack.getSeqNum() + 1) % 8;
-
-					if (nextSeqNum > pack.getSeqNum())
-					{
-						outstandingPacket = (nextSeqNum - base) % 8;
-					}
-					else
-					{
-						outstandingPacket = (N - (abs(nextSeqNum - ackseq))) % 8;
-					}
-				}
-
-
-				//printing contents
-				printf("SB: %d\n", base);
-				printf("NS: %d\n", nextSeqNum);
-				printf("Number of outstanding packets: %d\n", outstandingPacket);
-				printf("------------------------------------------------------\n");
-				ackLog << pack.getSeqNum() << endl;
-			}
-		}
-		//udpsocket error
-		else if (setTimer < 0)
-		{
-			std::cout << "Socket Error: " << strerror(errno) << " \n";
-			exit(1);
-		}
-		//timeout occured
-		else
-		{
-			cout << "\n**********Timeout Occured*******************" << endl;
-			cout << "Resending all packets....." << endl;
-
-
-			//resending the packet from same way as before
-			for (int i = (seqnum - 6) % 8; i < seqnum + 1; ++i)
-			{
-				if (data[i] != nullptr)
-				{
-					pack = packet(type, i, length, data[i]);
-
-					pack.serialize(dataPacket);
-
-					//send the message to server
-					sendto(udpSocket, dataPacket, sizeof(dataPacket), 0, (struct sockaddr *)&server, sizeof(server));
-					cout << "-----------------------------------------" << endl;
-					pack.printContents();
-					seqNumLog << pack.getSeqNum() << endl;
-				}
-			}
-		}
-	}
-
-	//closing the files and socket
-	seqNumLog.close();
-	ackLog.close();
-	close(udpSocket);
+  close(udp_socket); // close the socket
 }
